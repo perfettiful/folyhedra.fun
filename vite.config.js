1c0f09cb287f125e2ff163d76e488ac54d606df9
Nathan Perfetti <nathanperfetti@gmail.com>
2025-10-06 18:31:56 -0500

    Fix CSP for Cloudflare and base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/incomplete-tetrahedra/' : '/',
+  base: '/',
   server: {
     port: 3000,
     open: true,
